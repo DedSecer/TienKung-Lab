1c8cd4042b295d2a86838327efd228708d01eba6
DedSecer <z2465216809qq.com@icloud.com>
2026-01-01 20:30:20 +0800

    reduce amp_num_preload_transitions

--- legged_lab/envs/tienkung/sac_amp_run_cfg.py
+++ legged_lab/envs/tienkung/sac_amp_run_cfg.py
@@ -387,7 +387,7 @@ class TienKungSACRunAgentCfg(RslRlOnPolicyRunnerCfg):
     # AMP parameters (from paper Table II)
     amp_reward_coef = 2.0  # AMP reward coefficient (paper: 2.0)
     amp_motion_files = ["legged_lab/envs/tienkung/datasets/motion_amp_expert/run.txt"]
-    amp_num_preload_transitions = 20_000_000  # Preload transitions (paper: 2*10^7)
+    amp_num_preload_transitions = 2_000_000  # Reduced from 2*10^7 to save memory (expert data loaded dynamically)
     amp_task_reward_lerp = 0.4  # Task reward weight w_task=0.4, w_AMP=0.6 (paper Section III)
     amp_discr_hidden_dims = [1024, 512]  # Discriminator hidden dims (paper)
     min_normalized_std = [0.01, 0.01, 0.01] + [0.01] * 17  # Minimum normalized std (paper: [0.01, 0.01, 0.01])
